ed3e92e914324a4bab8698b79fa67c8d61d20c0f
kiritisagar <132217732+kiritisagar@users.noreply.github.com>
2024-07-21 22:20:03 +0530

    Update index.js

--- FRONTEND-REACT/src/index.js
+++ FRONTEND-REACT/src/index.js
@@ -1,12 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';  // Import global CSS file
-
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
-
+// src/index.js
+import './index.css';
